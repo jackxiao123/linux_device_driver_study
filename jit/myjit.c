d55a986ed30d164e69454b15138c90033dbfcdde
Jack Xiao <jack.xiao@outlook.com>
2021-04-26 09:24:22 +0800

    add busy

--- jit/myjit.c
+++ jit/myjit.c
@@ -35,39 +35,76 @@ static int myjit_current_time(struct seq_file *m, void *v)
     j2 = get_jiffies_64();
     do_gettimeofday(&tv1);
     tv2 = current_kernel_time();
-
     seq_printf(m, "jiffies is %ld\n", j1);
     seq_printf(m, "get_jiffies_64 is %ld\n", j2);
     seq_printf(m, "do_gettimeofday is %ld:%ld\n", tv1.tv_sec, tv1.tv_usec);
     seq_printf(m, "current_kernel_time is %ld:%ld\n", tv2.tv_sec, tv2.tv_nsec);
+
+    j1 = jiffies;
+    j2 = get_jiffies_64();
+    do_gettimeofday(&tv1);
+    tv2 = current_kernel_time();
+    seq_printf(m, "jiffies is %ld\n", j1);
+    seq_printf(m, "get_jiffies_64 is %ld\n", j2);
+    seq_printf(m, "do_gettimeofday is %ld:%ld\n", tv1.tv_sec, tv1.tv_usec);
+    seq_printf(m, "current_kernel_time is %ld:%ld\n", tv2.tv_sec, tv2.tv_nsec);
+
     return 0;
 }
 
-static int myjit_open(struct inode *inode, struct file *filp)
+static int myjit_current_time_open(struct inode *inode, struct file *filp)
 {
     return single_open(filp, myjit_current_time, NULL);
 }
 
-static struct file_operations myops =
+static struct file_operations mypos_current_time =
 {
     .owner = THIS_MODULE,
-    .open = myjit_open,
+    .open = myjit_current_time_open,
     .read = seq_read,
     .release = single_release,
 };
+
+static int myjit_busy(struct seq_file *m, void *v)
+{
+    seq_printf(m, "before busy 10s:%ld\n", jiffies);
+    unsigned long j1 = jiffies + 10000;
+    while (time_before(jiffies, j1)) cpu_relax();
+    seq_printf(m, "after busy 10s:%ld\n", jiffies);
+    return 0;
+}
+
+static int myjit_busy_open(struct inode *inode, struct file *filp)
+{
+    return single_open(filp, myjit_busy, NULL);
+}
+
+static struct file_operations mypos_busy =
+{
+    .owner = THIS_MODULE,
+    .open = myjit_busy_open,
+    .read = seq_read,
+    .release = single_release,
+};
+
+static struct proc_dir_entry * parent = NULL;
 
 static int hello_init(void)
 {
     printk(KERN_ALERT "Hello, world - just in time\n");
 
-    struct proc_dir_entry* parent = proc_mkdir("myjit", NULL);
-    proc_create("currentime", 0, parent, &myops);
+    parent = proc_mkdir("myjit", NULL);
+    proc_create("currentime", 0, parent, &mypos_current_time);
+    proc_create("busy", 0, parent, &mypos_busy);
 
     return 0;
 }
 
 static void hello_exit(void)
 {
+    remove_proc_entry("currentime", parent);
+    remove_proc_entry("busy", parent);
+    proc_remove(parent);
     printk(KERN_ALERT "Goodbye, cruel world - just in time\n");
 }
 
