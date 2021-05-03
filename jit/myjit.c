15aee5074212480b4bcf6a1c86c0331a90667e6d
Jack Xiao <jack.xiao@outlook.com>
2021-05-03 10:30:17 +0800

    add tasklet code

--- jit/myjit.c
+++ jit/myjit.c
@@ -21,6 +21,8 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 #include <linux/wait.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("jackx");
@@ -38,7 +40,7 @@ static int myjit_current_time(struct seq_file *m, void *v)
     do_gettimeofday(&tv1);
     tv2 = current_kernel_time();
     seq_printf(m, "jiffies is %ld\n", j1);
-    seq_printf(m, "get_jiffies_64 is %ld\n", j2);
+    seq_printf(m, "get_jiffies_64 is %ld\n", (long)j2);
     seq_printf(m, "do_gettimeofday is %ld:%ld\n", tv1.tv_sec, tv1.tv_usec);
     seq_printf(m, "current_kernel_time is %ld:%ld\n", tv2.tv_sec, tv2.tv_nsec);
 
@@ -47,7 +49,7 @@ static int myjit_current_time(struct seq_file *m, void *v)
     do_gettimeofday(&tv1);
     tv2 = current_kernel_time();
     seq_printf(m, "jiffies is %ld\n", j1);
-    seq_printf(m, "get_jiffies_64 is %ld\n", j2);
+    seq_printf(m, "get_jiffies_64 is %ld\n", (long)j2);
     seq_printf(m, "do_gettimeofday is %ld:%ld\n", tv1.tv_sec, tv1.tv_usec);
     seq_printf(m, "current_kernel_time is %ld:%ld\n", tv2.tv_sec, tv2.tv_nsec);
 
@@ -92,8 +94,9 @@ static struct file_operations mypos_busy =
 
 static int myjit_schedule(struct seq_file *m, void *v)
 {
+    unsigned long j1;
     seq_printf(m, "before schedule 10s:%ld\n", jiffies);
-    unsigned long j1 = jiffies + 10000;
+    j1 = jiffies + 10000;
     while (time_before(jiffies, j1)) schedule();
     seq_printf(m, "after schedule 10s:%ld\n", jiffies);
     return 0;
@@ -140,7 +143,7 @@ static struct file_operations mypos_queue =
 #define JIT_ASYNC_LOOPS 20 
 #define JIT_DELAY 1000
 
-static struct jitimer_data
+struct jitimer_data
 {
     unsigned long prev_jiffies;
     struct seq_file *m;
@@ -149,14 +152,14 @@ static struct jitimer_data
     int count;
 };
 
-struct jitimer_data mydata;
+static struct jitimer_data my_timer_data;
 
 static void jit_timer_fn(struct timer_list *t)
 {
     struct jitimer_data *data = container_of(t, struct jitimer_data, t); 
     unsigned long now = jiffies;
 
-    seq_printf(data->m, "%10ld %6d %6ld %9d %9d %3d %-30s\n",
+    seq_printf(data->m, "%10ld %6ld %6ld %9d %9d %3d %-30s\n",
         now, (long)(now - data->prev_jiffies), in_interrupt(), in_atomic(), task_pid_nr(current), smp_processor_id(), current->comm);
     data->count--;
     if (data->count > 0)
@@ -175,22 +178,22 @@ static int myjit_timer(struct seq_file *m, void *v)
     int ret;
     unsigned long now = jiffies;
    
-    timer_setup(&mydata.t, jit_timer_fn, 0);
-    mydata.t.expires = now + JIT_DELAY;
-    mydata.prev_jiffies = now;
-    mydata.m = m;
-    mydata.count = JIT_ASYNC_LOOPS;
+    timer_setup(&my_timer_data.t, jit_timer_fn, 0);
+    my_timer_data.t.expires = now + JIT_DELAY;
+    my_timer_data.prev_jiffies = now;
+    my_timer_data.m = m;
+    my_timer_data.count = JIT_ASYNC_LOOPS;
 
     seq_printf(m, "%10s %6s %6s %9s %9s %3s %-30s\n",
         "time", "delta", "inirq", "inatomic", "pid", "cpu", "cmd");
     seq_printf(m, "%10ld %6d %6ld %9d %9d %3d %-30s\n",
         now, 0, in_interrupt(), in_atomic(), task_pid_nr(current), smp_processor_id(), current->comm);
 
-    add_timer(&mydata.t);
+    add_timer(&my_timer_data.t);
 
-    while (mydata.count > 0)
+    while (my_timer_data.count > 0)
     {
-        if (wait_event_interruptible(mydata.qwait, mydata.count == 0))
+        if (wait_event_interruptible(my_timer_data.qwait, my_timer_data.count == 0))
         {
             printk("myjit wait_event_interruptible fail\n");
             ret = -ERESTARTSYS;
@@ -199,8 +202,8 @@ static int myjit_timer(struct seq_file *m, void *v)
     }
     ret = 0;
 cleanup:
-   mydata.count = 0;
-    del_timer_sync(&mydata.t);
+   my_timer_data.count = 0;
+    del_timer_sync(&my_timer_data.t);
     return ret;
 }
 
@@ -217,6 +220,83 @@ static struct file_operations mypos_timer =
     .release = single_release,
 };
 
+struct jitasklet_data
+{
+    unsigned long prev_jiffies;
+    struct seq_file *m;
+    struct tasklet_struct t;
+    wait_queue_head_t qwait;
+    int count;
+};
+
+static struct jitasklet_data my_tasklet_data;
+
+static void jit_tasklet_fn(unsigned long arg)
+{
+    struct jitasklet_data *data = (struct jitasklet_data *)arg; 
+    unsigned long now = jiffies;
+
+    seq_printf(data->m, "%10ld %6ld %6ld %9d %9d %3d %-30s\n",
+        now, (long)(now - data->prev_jiffies), in_interrupt(), in_atomic(), task_pid_nr(current), smp_processor_id(), current->comm);
+    data->count--;
+    if (data->count > 0)
+    {
+        data->prev_jiffies = now;
+        tasklet_schedule(&data->t);
+    }
+    else
+    {
+        wake_up_interruptible(&data->qwait);
+    }
+}
+
+static int myjit_tasklet(struct seq_file *m, void *v)
+{
+    int ret;
+    unsigned long now = jiffies;
+   
+    tasklet_init(&my_tasklet_data.t, jit_tasklet_fn, (unsigned long)(&my_tasklet_data));
+    my_tasklet_data.prev_jiffies = now;
+    my_tasklet_data.m = m;
+    my_tasklet_data.count = JIT_ASYNC_LOOPS;
+
+    seq_printf(m, "%10s %6s %6s %9s %9s %3s %-30s\n",
+        "time", "delta", "inirq", "inatomic", "pid", "cpu", "cmd");
+    seq_printf(m, "%10ld %6d %6ld %9d %9d %3d %-30s\n",
+        now, 0, in_interrupt(), in_atomic(), task_pid_nr(current), smp_processor_id(), current->comm);
+
+    msleep_interruptible(3000);
+
+    tasklet_schedule(&my_tasklet_data.t);
+
+    while (my_tasklet_data.count > 0)
+    {
+        if (wait_event_interruptible(my_tasklet_data.qwait, my_tasklet_data.count == 0))
+        {
+            printk("myjit_tasklet wait_event_interruptible fail\n");
+            ret = -ERESTARTSYS;
+            goto cleanup;
+        }
+    }
+    ret = 0;
+cleanup:
+    my_tasklet_data.count = 0;
+    tasklet_kill(&my_tasklet_data.t);
+    return ret;
+}
+
+static int myjit_tasklet_open(struct inode *inode, struct file *filp)
+{
+    return single_open(filp, myjit_tasklet, NULL);
+}
+
+static struct file_operations mypos_tasklet =
+{
+    .owner = THIS_MODULE,
+    .open = myjit_tasklet_open,
+    .read = seq_read,
+    .release = single_release,
+};
 static struct proc_dir_entry * parent = NULL;
 
 static int hello_init(void)
@@ -229,8 +309,10 @@ static int hello_init(void)
     proc_create("sched", 0, parent, &mypos_schedule);
     proc_create("queue", 0, parent, &mypos_queue);
     proc_create("timer", 0, parent, &mypos_timer);
+    proc_create("tasklet", 0, parent, &mypos_tasklet);
 
-    init_waitqueue_head(&mydata.qwait);
+    init_waitqueue_head(&my_timer_data.qwait);
+    init_waitqueue_head(&my_tasklet_data.qwait);
 
     return 0;
 }
